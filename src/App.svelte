fd64c9834aab0ede4638f41ee241cdabb87612c7
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-09-13 19:32:40 +0200

    Header component

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,9 @@
 <script>
+  import Header from "./lib/Header.svelte";
 </script>
 
 <main>
-  <header>
-    <div class="container">
-      <h1>Hey, I'm Matteo</h1>
-      <p>
-        A web developer with an eye for design from Milan, Italy. Instead of the
-        traditional portfolio site that never gets updated I wanted to make
-        something more simple and personal as a collection of things I enjoy.
-      </p>
-    </div>
-  </header>
+  <Header />
 </main>
 
 <style global lang="scss">
---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<header>
+  <div class="container">
+    <h1>Hey, I'm Matteo</h1>
+    <p>
+      A web developer with an eye for design from Milan, Italy. Instead of the
+      traditional portfolio site that never gets updated I wanted to make
+      something more simple and personal as a collection of things I enjoy.
+    </p>
+  </div>
+</header>
