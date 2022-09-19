fdcf547eb2c37e3ff79bd53a5f7b2c7cb21c769c
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-09-19 09:25:52 +0200

    update header text

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,7 +4,8 @@
     <p>
       A web developer with an eye for design from Milan, Italy. Instead of the
       traditional portfolio site that never gets updated I wanted to make
-      something more simple and personal as a collection of things I enjoy.
+      something more simple and personal as a collection of things I enjoy. My
+      passions are coding, CrossFit, books, music and travel.
     </p>
   </div>
 </header>
