059433b39fdf1f6ce7be0f310548c6df1dd31c27
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-10-25 11:43:41 +0200

    revert to static

--- src/lib/Books.svelte
+++ src/lib/Books.svelte
@@ -1,63 +1,57 @@
 <script>
-  import { onMount } from "svelte";
-  import axios from "axios";
-  import Loader from "./Loader.svelte";
+  const nonfiction = [
+    {
+      title: "The Warmth of Other Suns",
+      author: "Isabel Wilkerson",
+      link: "https://www.goodreads.com/book/show/8171378-the-warmth-of-other-suns",
+    },
+    {
+      title: "Questa è l'America",
+      author: "Francesco Costa",
+      link: "https://www.goodreads.com/book/show/49402100-questa-l-america",
+    },
+    {
+      title: "Grand Central Winter: Stories from the Street",
+      author: "Lee Stringer",
+      link: "https://www.goodreads.com/book/show/63651.Grand_Central_Winter",
+    },
+  ];
 
-  const RSS_URL =
-    "https://rss-to-json-serverless-api.vercel.app/api?feedURL=https://oku.club/rss/collection/";
-  const reading = "NDdVp";
-  const read = "lHlql";
-
-  let readingBooks = [];
-  let readBooks = [];
-  onMount(async () => {
-    const response = await axios.get(RSS_URL + reading);
-    const response2 = await axios.get(RSS_URL + read);
-    readingBooks = response.data.items;
-    readingBooks = readingBooks.slice(0, 2);
-    readBooks = response2.data.items;
-    readBooks = readBooks.slice(0, 3);
-  });
+  const fiction = [
+    {
+      title: "East of Eden",
+      author: "John Steinbeck",
+      link: "https://www.goodreads.com/book/show/4406.East_of_Eden",
+    },
+    {
+      title: "Brothers",
+      author: "Yu Hua",
+      link: "https://www.goodreads.com/book/show/3920295-brothers",
+    },
+  ];
 </script>
 
 <div>
-  <p>
-    Looking for book to read? Check out some pulled from my <a
-      href="https://oku.club/user/mattetgl"
-      target="_blank">Oku.club</a
-    > feed.
-  </p>
+  <p>Looking for a new book to read? Check out some of my favorites.</p>
   <!-- Novels -->
   <div>
-    <p class="status reading">
-      <img src="/reading.svg" alt="reading" /> Reading...
-    </p>
-    {#if readingBooks.length > 0}
-      {#each readingBooks as book}
-        <a href={book.link} class="book" target="_blank">
-          <h3>{book.title}</h3>
-          <small>{book.author.$t}</small>
-        </a>
-      {/each}
-    {:else}
-      <Loader />
-    {/if}
+    <p class="status nonfiction">Non-fiction</p>
+    {#each nonfiction as { title, author, link }}
+      <a href={link} class="book" target="_blank">
+        <h3>{title}</h3>
+        <small>{author}</small>
+      </a>
+    {/each}
   </div>
   <!-- Read -->
   <div>
-    <p class="status read">
-      <img src="/read.svg" alt="read" /> Last finished
-    </p>
-    {#if readBooks.length > 0}
-      {#each readBooks as book}
-        <a href={book.link} class="book" target="_blank">
-          <h3>{book.title}</h3>
-          <small>{book.author.$t}</small>
-        </a>
-      {/each}
-    {:else}
-      <Loader />
-    {/if}
+    <p class="status fiction">Fiction</p>
+    {#each fiction as { title, author, link }}
+      <a href={link} class="book" target="_blank">
+        <h3>{title}</h3>
+        <small>{author}</small>
+      </a>
+    {/each}
   </div>
 </div>
 
@@ -79,12 +73,12 @@
     gap: 0.25rem;
     border-radius: 0.25rem;
 
-    &.reading {
+    &.nonfiction {
       color: $reading-text;
       background-color: $reading-background;
     }
 
-    &.read {
+    &.fiction {
       color: $read-text;
       background-color: $read-background;
     }
