f3a06d7d335a009bb223034d10fe7e1a1e57a18b
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-01-07 19:13:28 +0100

    works and now sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Header from "./lib/Header.svelte";
+  import Works from "./lib/Works/Index.svelte";
 </script>
 
 <div
   class="children:w-full children:mx-auto children:px-4 space-y-10 sm:space-y-20"
 >
   <Header />
+  <Works />
 </div>
---
+++ src/lib/Works/Now.svelte
@@ -0,0 +1,7 @@
+<p class="text-md">
+  Coding and designing at <span class="font-semibold">TI3</span> with Svelte, Tailwind,
+  Laravel and Figma.
+</p>
+<p class="text-md">
+  Experimenting on my own with SvelteKit, Astro and Supabase.
+</p>
