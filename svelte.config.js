ace77ce255d0810c4c0157858e60a3f6ea37af79
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-05 17:22:12 +0100

    rm unused option in config

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,6 @@ import preprocess from "svelte-preprocess";
 const config = {
   preprocess: [
     preprocess({
-      scss: {
-        prependData: '@use "src/variables.scss" as *;',
-      },
-
       postcss: true,
     }),
   ],
