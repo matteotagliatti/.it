519c2a77005912d996a8138d8a254f4e545e4a57
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-05 17:18:33 +0100

    update now

--- src/lib/Works/Now.svelte
+++ src/lib/Works/Now.svelte
@@ -1,6 +1,7 @@
 <p class="text-md">
-  Coding and designing at <span class="font-semibold">TI3</span> with Svelte, Tailwind,
-  Laravel and Figma.
+  Leader Frontend Developer and Designer at <span class="font-semibold"
+    >TI3</span
+  > working with Svelte, Tailwind, Laravel and Figma.
 </p>
 <p class="text-md">
   Experimenting on my own with SvelteKit, Astro and Supabase. All my stuff is on <a
