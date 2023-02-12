3c97a07b669469c51ea37f123f0eea37c63829d1
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-02-12 10:22:57 +0100

    books and movies static

--- src/lib/Activity/Books.svelte
+++ src/lib/Activity/Books.svelte
@@ -1,53 +1,53 @@
 <script>
-  import { onMount } from "svelte";
-  import axios from "axios";
-  import Loader from "../Loader.svelte";
-
-  const XMLtoApi = "https://rss-to-json-serverless-api.vercel.app/api?feedURL=";
-  const OkuRSSLink = "https://oku.club/rss/collection/75aEs";
-  let books;
-
-  onMount(async () => {
-    try {
-      books = await axios
-        .get(XMLtoApi + OkuRSSLink)
-        .then((res) => res.data.items);
-      if (books.length > 5) {
-        books = books.slice(0, 6);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  const books = [
+    {
+      title: "East of Eden",
+      author: "John Steinbeck",
+      link: "https://www.goodreads.com/book/show/4406.East_of_Eden",
+    },
+    {
+      title: "Brothers",
+      author: "Yu Hua",
+      link: "https://www.goodreads.com/book/show/3920295-brothers",
+    },
+    {
+      title: "The Warmth of Other Suns",
+      author: "Isabel Wilkerson",
+      link: "https://www.goodreads.com/book/show/8171378-the-warmth-of-other-suns",
+    },
+    {
+      title: "Grand Central Winter: Stories from the Street",
+      author: "Lee Stringer",
+      link: "https://www.goodreads.com/book/show/63651.Grand_Central_Winter",
+    },
+    {
+      title: "KLF: Chaos Magic Music Money",
+      author: "John Higgs",
+      link: "https://www.goodreads.com/book/show/16166062-klf",
+    },
+    {
+      title: "The Stories of Breece D'J Pancake",
+      author: "Breece D'J Pancake",
+      link: "https://www.goodreads.com/book/show/52762.The_Stories_of_Breece_D_J_Pancake",
+    },
+  ];
 </script>
 
 <section class="flex flex-col gap-4 sm:basis-2/5">
-  <p>
-    Looking for a book to read? Check out the last few I've finished pulled from
-    my <a
-      href="https://oku.club/user/mattetgl"
-      class="underline text-indigo-600 dark:text-indigo-400"
-      rel="noopener noreferrer"
-      target="_blank">Oku</a
-    > feed.
-  </p>
-  {#if books}
-    <div class="flex flex-col gap-4">
-      {#each books as book}
-        <a
-          href={book.link}
-          target="_blank"
-          class="group"
-          rel="noopener noreferrer"
-        >
-          <h2 class="group-hover:underline font-semibold">{book.title}</h2>
-          <p class="text-slate-500 text-sm dark:text-gray-400">
-            {book.author.$t}
-          </p>
-        </a>
-      {/each}
-    </div>
-  {:else}
-    <Loader />
-  {/if}
+  <p>Looking for a book to read? Check out some of my favorites below.</p>
+  <div class="flex flex-col gap-4">
+    {#each books as book}
+      <a
+        href={book.link}
+        target="_blank"
+        class="group"
+        rel="noopener noreferrer"
+      >
+        <h2 class="group-hover:underline font-semibold">{book.title}</h2>
+        <p class="text-slate-500 text-sm dark:text-gray-400">
+          {book.author}
+        </p>
+      </a>
+    {/each}
+  </div>
 </section>
