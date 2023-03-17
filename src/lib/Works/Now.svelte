08d0b357deef2a2409ab4c208d20bc8b3f6b9e28
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-17 13:29:54 +0100

    update now

--- src/lib/Works/Now.svelte
+++ src/lib/Works/Now.svelte
@@ -4,8 +4,8 @@
   > working on industrial software with Svelte, Tailwind, Laravel and Figma.
 </p>
 <p class="text-md">
-  Experimenting on my own with React, Astro, Node and Supabase. All my stuff is
-  on <a
+  Really exited about serverless stuff. Experimenting on my own with React,
+  Astro, Node, Supabase and Pocketbase. All my stuff is on <a
     class="font-semibold hover:underline"
     href="https://github.com/matteotagliatti">GitHub</a
   >.
