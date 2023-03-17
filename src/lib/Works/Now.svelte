474b0562f6584f40fbac948f76b9cfbede1eb849
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-17 13:39:30 +0100

    more links

--- src/lib/Works/Now.svelte
+++ src/lib/Works/Now.svelte
@@ -1,12 +1,40 @@
 <p class="text-md">
-  Leader Frontend Developer and Designer at <span class="font-semibold"
-    >TI3</span
+  Leader Frontend Developer and Designer at <a
+    href="https://www.linkedin.com/search/results/all/?keywords=TI3&sid=YLg"
+    class="font-semibold"
+    target="_blank">TI3</a
   > working on industrial software with Svelte, Tailwind, Laravel and Figma.
 </p>
 <p class="text-md">
-  Really exited about serverless stuff. Experimenting on my own with React,
-  Astro, Node, Supabase and Pocketbase. All my stuff is on <a
+  Really exited about serverless stuff. Experimenting on my own with <a
+    href="https://react.dev/"
     class="font-semibold hover:underline"
-    href="https://github.com/matteotagliatti">GitHub</a
+    target="_blank">React</a
+  >,
+  <a
+    href="https://astro.build/"
+    class="font-semibold hover:underline"
+    target="_blank">Astro</a
+  >,
+  <a
+    href="https://nodejs.org/"
+    class="font-semibold hover:underline"
+    target="_blank">Node.js</a
+  >,
+  <a
+    href="https://supabase.com/"
+    class="font-semibold hover:underline"
+    target="_blank">Supabase</a
+  >
+  and
+  <a
+    href="https://pocketbase.io/"
+    class="font-semibold hover:underline"
+    target="_blank">Pocketbase</a
+  >. All my stuff is on
+  <a
+    class="font-semibold hover:underline"
+    href="https://github.com/matteotagliatti"
+    target="_blank">GitHub</a
   >.
 </p>
