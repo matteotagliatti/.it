b87974300eb81d6985b86ae76ed2d31cb26e8e2e
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-04-11 00:40:23 +0200

    upgrade now

--- src/components/Works/Now.svelte
+++ src/components/Works/Now.svelte
@@ -7,37 +7,8 @@
   > working on industrial software with Svelte, Tailwind, Laravel and Figma.
 </p>
 <p class="text-md">
-  Really exited about serverless stuff. Experimenting on my own with <a
-    href="https://react.dev/"
-    class="font-semibold hover:underline"
-    target="_blank"
-    rel="noopener noreferrer">React</a
-  >,
-  <a
-    href="https://astro.build/"
-    class="font-semibold hover:underline"
-    target="_blank"
-    rel="noopener noreferrer">Astro</a
-  >,
-  <a
-    href="https://nodejs.org/"
-    class="font-semibold hover:underline"
-    target="_blank"
-    rel="noopener noreferrer">Node.js</a
-  >,
-  <a
-    href="https://supabase.com/"
-    class="font-semibold hover:underline"
-    target="_blank"
-    rel="noopener noreferrer">Supabase</a
-  >
-  and
-  <a
-    href="https://pocketbase.io/"
-    class="font-semibold hover:underline"
-    target="_blank"
-    rel="noopener noreferrer">Pocketbase</a
-  >. All my stuff is on
+  Exited about serverless stuff. Experimenting on my own with React, Astro, Node
+  and Supabase. All my stuff is on
   <a
     class="font-semibold hover:underline"
     href="https://github.com/matteotagliatti"
