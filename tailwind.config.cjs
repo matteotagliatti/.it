816ac970c1a737c46de7d0cc45935522b813078d
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-05-11 13:27:47 +0200

    init astro, add tailwind, inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,11 @@
-const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
       sans: ["Inter", "sans-serif"],
     },
     extend: {},
   },
-
-  plugins: [
-    require("@tailwindcss/line-clamp"),
-    function ({ addVariant }) {
-      addVariant("children", "& > *");
-    },
-  ],
+  plugins: [],
 };
-
-module.exports = config;
