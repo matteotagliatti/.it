5a9b6794be7f6cd152728b4a46047b9a6a11ce9c
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-11-19 19:51:15 +0100

    better font import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Inter", "sans-serif"],
-    },
-
     extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+
       colors: {
         background: "#050505",
         bubble: "#fafcff14",
