5afae89fcbc0b80ca68e9df02627a753ccdee0a4
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-01-13 20:07:03 +0100

    better highlights, update astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()]
 });
