792fcebfbe3a455462389a36f0a381162d857d47
Matteo Tagliatti <matteotagliatti@gmail.com>
2024-01-14 04:19:55 +0100

    rm mdx dependancy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind()],
 });
